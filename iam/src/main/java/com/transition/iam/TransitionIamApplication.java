5aeb80d9f8de33f9d612a50e265967ee56d94b80
ndeya97 <nadiop97@gmail.com>
2025-08-22 18:16:48 -0600

    feat: add Spring Boot project (iam)

---
+++ iam/src/main/java/com/transition/iam/TransitionIamApplication.java
@@ -0,0 +1,13 @@
+package com.transition.iam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitionIamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransitionIamApplication.class, args);
+	}
+
+}
---
+++ iam/src/test/java/com/transition/iam/TransitionIamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transition.iam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitionIamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
